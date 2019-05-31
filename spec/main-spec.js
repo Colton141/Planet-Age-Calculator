421e01675508014cfa604b064bf855b118e9ca78
colton lacey <coltnutmiles14@gmail.com>
2019-05-31 14:02:12 -0700

    add test for age on venus function

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -8,6 +8,11 @@ test('should return age on mercury', () => {
     expect(agemercury(20)).toBe(47);
 });
 
+test('should return age on venus', () => {
+    expect(ageVenus(15)).toBe(33);
+});
+
+
 test('adds 5 + 5 to equal 10', () => {
     expect(add(5, 5)).toBe(10);
 });
