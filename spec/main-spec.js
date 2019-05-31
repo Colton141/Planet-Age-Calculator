81a75f70035ba55e94336c64da36b3aad804d7cd
colton lacey <coltnutmiles14@gmail.com>
2019-05-31 10:25:44 -0700

    add date testt

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,9 @@
 import { add } from '../src/functions';
+import { Date } from '../src/functions';
 
+test('date object should build', () => {
+  epect (Date(2019, 10, 28).toBe(Date.year))
+})
 test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
 });
